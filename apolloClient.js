ffc03958d0256895c15134a37cda9da2508c1e5f
Claude Bejjani <cbejjani@gmail.com>
2024-12-23 01:06:27 +0200

    bo

--- apolloClient.js
+++ apolloClient.js
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://be121.onetoonefit.com/121/graphql',
+  uri: 'https://bo.onetoonefit.com/121/graphql',
   cache: new InMemoryCache(),
 });
 
